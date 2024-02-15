0351d8a3a4104181cc4dbf135fabae4562a52b64
k-oyun <dhdbs001208@naver.com>
2024-02-15 14:27:58 +0900

    set Selectors
    set? - 이 속성이 설정되면 selector는 쓰기 가능한 상태를 반환한다. 첫번째 매개변수로 콜백 객체와 새로 입력 값이 전달된다. 사용자가 selector를 재설정할 경우 새로 입력 값은 T 타입의 값 또는 DefaultValue 타입의 객체일 수 있다. 콜백에는 다음이 포함된다.
    
    get 매개변수
    다른 atom이나 selector로부터 값을 찾는데 사용되는 함수. 이 함수는 selector를 주어진 atom이나 selector를 구독하지 않는다.
    
    set 매개변수
    업스트림 Recoil 상태의 값을 설정할 때 사용되는 함수. 첫 번째 매개변수는 Recoil state, 두 번째 매개변수는 새로운 값(newValue)이다. 새로운 값은 업데이트 함수나 재설정 액션을 전파하는 DefalutValue 객체일 수 있다.

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,14 +1,36 @@
-import { createGlobalStyle } from "styled-components";
-import ToDoList from "./components/ToDoList";
-
-const GlobalStyle = createGlobalStyle``;
+import { useRecoilState } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
+  //userecoilstate atom의 값을 불러오거나 수정할 수 있음
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+
+  //selector에서의 set함수와 사용
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    //+를 추가함으로써 string을 numberfh qkRNa
+    setMinutes(+event.currentTarget.value);
+  };
+
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
   return (
-    <>
-      <GlobalStyle />
-      <ToDoList />
-    </>
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
+    </div>
   );
 }
 
--- myapp/src/atoms.tsx
+++ myapp/src/atoms.tsx
@@ -1,37 +1,23 @@
 import { atom, selector } from "recoil";
 
-//enum으로 만들어 사용하면 오타가 날 확률이 줄어든다!
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const categoryState = atom<Categories>({
-  key: "categoty",
-  default: Categories.TO_DO,
-});
+//state를 가져다가 수정한 output을 리턴함 (selector)
+export const hourSelector = selector<number>({
+  key: "hours",
 
-//state에 맞게 배열에 저장
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  //get function으로 atoms의 요소를 가져올 수 있음
+  //atom의 값을 가져오는 get function
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    //조건에 맞지않는 원소를 제거
-    //각각 조건에 따라 배열에 저장
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+
+  //atom의 state를 수정할 수 있음
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
