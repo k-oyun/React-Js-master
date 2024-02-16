1edb7a88f77a9d17ea5660fd9de996c0e9215f8b
k-oyun <dhdbs001208@naver.com>
2024-02-16 10:29:01 +0900

    use framer-motion
    Animation
    Framer Motion의 애니메이션은 모션 컴포넌트의 유연한 animate 속성을 통해 제어됩니다. 간단한 애니메이션의 경우 animate props에서 직접 값을 설정할 수 있습니다.
    ex) motion.div animate={{ rotate: 360 }} transition={{ duration: 2 }}
    https://www.framer.com/docs/animation
    
    initial: boolean | Target | VariantLabels (애니메이션의 초기값 지정)
    속성, 변형 레이블 또는 시작할 변형 레이블의 배열입니다.
    animate의 값으로 초기화하려면 false로 설정합니다(마운트 애니메이션 비활성화).
    https://www.framer.com/docs/component/###initial
    
    Transition
    Transition은 값이 한 상태에서 다른 상태로 움직이는 방식을 정의합니다.
    또한 Tween, Spring 또는 Inertia를 사용할 애니메이션 유형을 정의하는 소품을 허용할 수 있습니다.
    ex) motion.div animate={{ rotate: 180 }} transition={{ type: 'spring' }}
    https://www.framer.com/docs/transition

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,83 +1,29 @@
-import { DragDropContext, DropResult } from "@hello-pangea/dnd";
-import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { toDoState } from "./atoms";
-import Board from "./components/Board";
-
+import { motion } from "framer-motion";
 const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
   display: flex;
-  max-width: 680px;
-  width: 100%;
-  margin: 0 auto;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 `;
-
-const Boards = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
-  width: 100%;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDrageEnd = (info: DropResult) => {
-    const { destination, draggableId, source } = info;
-
-    if (!destination) return;
-
-    if (destination?.droppableId === source.droppableId) {
-      // same board movement.
-      //splice는 array에서 한 부분을 수정하고 변형시킴
-      setToDos((allBoards) => {
-        //toDo들을 복사해옴 (spread연산자로 만드는 이유 : 완전히 새로운 배열 생성을 위함)
-        const boardCopy = [...allBoards[source.droppableId]];
-
-        //todo의 정보를 받아옴
-        const taskObj = boardCopy[source.index];
-        // 1) delete item on source.index
-        boardCopy.splice(source.index, 1);
-        // 2) aput back the  item on destination.index
-        boardCopy.splice(destination?.index, 0, taskObj);
-
-        //이전거에 새로운 보드를 추가해주는 것
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    }
-
-    if (destination.droppableId !== source.droppableId) {
-      //cross board movement
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        // 1) delete item on source.index
-        sourceBoard.splice(source.index, 1);
-        // 2) aput back the  item on destination.index
-        destinationBoard.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
   return (
-    <DragDropContext onDragEnd={onDrageEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
-          ))}
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
+    <Wrapper>
+      <Box
+        transition={{ type: "spring", delay: 1 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
+    </Wrapper>
   );
 }
 
