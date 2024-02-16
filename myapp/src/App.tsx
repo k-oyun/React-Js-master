996004a46d6f4e1ec1e74548385d06b80489384b
k-oyun <dhdbs001208@naver.com>
2024-02-16 13:56:01 +0900

    SVG Animation
    let's go ILSAN~~

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,11 +1,6 @@
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+
 const Wrapper = styled(motion.div)`
   height: 200vh;
   width: 100vw;
@@ -14,48 +9,45 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  path {
+    stroke: white;
+    strok-width: 2;
+  }
 `;
 
-const boxVariants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
+const svg = {
+  //pathlength를 통해 svg의 테두리 나타남을 조절할 수 있음
+  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
+  end: {
+    fill: "rgba(255, 255, 255, 1)",
+    pathLength: 1,
+  },
 };
 
 function App() {
-  //x를 지속적으로 추적
-  //컴포넌트가 움직이더라도 재렌더링하지않음 -> 성능향상
-  const x = useMotionValue(0);
-
-  //-800~800으로이동시 컴포넌트를 360도 돌림
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  //useEffect와 같이 사용해야 확인할 수 있음
-  useEffect(() => {}, [x]);
-
-  //-800~800을 이동하면 백그라운드 색상이 변함
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 8, 238))",
-      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-    ]
-  );
-
-  //scroll을 내려갈수록 박스가 작아짐
-  const { scrollYProgress } = useViewportScroll();
-
-  //지정범위에따라 박스의 크기가 달라짐
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper>
+      <Svg
+        focusable="false"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <motion.path
+          variants={svg}
+          initial="start"
+          animate="end"
+          //특정 요소의 transition시간을 정할 수 있음
+          transition={{
+            default: { duration: 5 },
+            //모든 transition은 5초지만 fill의 duration만 3초로 개별 지정
+            fill: { duration: 1, delay: 3 },
+          }}
+          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        ></motion.path>
+      </Svg>
     </Wrapper>
   );
 }
