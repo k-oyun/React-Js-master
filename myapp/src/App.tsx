eb2e83458d9d8be00c1c2f410cb1a2c52e80bdb5
k-oyun <dhdbs001208@naver.com>
2024-02-19 15:48:10 +0900

    Slider part Two

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -25,52 +25,63 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  invisible: {
-    x: 500,
+  //box의 custom prop을 받아 true이면 500 false이면 -500적용
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
     transition: {
-      duration: 1,
+      duration: 0.3,
     },
   },
-  exit: {
-    x: -500,
+  //box의 custom prop을 받아 true이면 -500 false이면 500적용
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
     opacity: 0,
     scale: 0,
     transition: {
-      duration: 1,
+      duration: 0.3,
     },
-  },
+  }),
 };
 
 function App() {
   const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
   //i가 10이면 10을 리턴
   //그렇지 않으면 1을 합한 값을 출력
-  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <AnimatePresence>
+      {/* mode wait은 하나의 효과가 끝난 후에 다음 효과가 시작되도록함 */}
+      {/* mode="wait" */}
+      <AnimatePresence custom={back}>
         {/* map에서 하나씩 매핑함 */}
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
-            <Box
-              variants={boxVariants}
-              initial="invisible"
-              animate="visible"
-              exit="exit"
-              key={i}
-            >
-              {i}
-            </Box>
-          ) : null
-        )}
+        {/* 기존 방식처럼 배열을 만드는게 아닌 usestate의 요소를 사용 */}
+        {/* react js는 새로운 요소를 만든다고 생각함 */}
+        <Box
+          //커스텀을 사용하여 상황에 따라 기존에 작성한 애니메이션 코드를 적용할 수 있음
+          custom={back}
+          variants={boxVariants}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
       <button onClick={nextPlease}>next</button>
       <button onClick={prevPlease}>prev</button>
