7b693c486b0bd3fdad02b46fd42a33049cb7f012
k-oyun <dhdbs001208@naver.com>
2024-02-19 16:45:53 +0900

    Final Project part One

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -2,89 +2,61 @@ import styled from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  height: 200px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
   position: absolute;
-  top: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  //box의 custom prop을 받아 true이면 500 false이면 -500적용
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  //box의 custom prop을 받아 true이면 -500 false이면 500적용
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
-};
-
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  //i가 10이면 10을 리턴
-  //그렇지 않으면 1을 합한 값을 출력
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggle = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      {/* mode wait은 하나의 효과가 끝난 후에 다음 효과가 시작되도록함 */}
-      {/* mode="wait" */}
-      <AnimatePresence custom={back}>
-        {/* map에서 하나씩 매핑함 */}
-        {/* 기존 방식처럼 배열을 만드는게 아닌 usestate의 요소를 사용 */}
-        {/* react js는 새로운 요소를 만든다고 생각함 */}
-        <Box
-          //커스텀을 사용하여 상황에 따라 기존에 작성한 애니메이션 코드를 적용할 수 있음
-          custom={back}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
+    <Wrapper onClick={toggle}>
+      <Grid>
+        <Box layoutId="hello" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked ? (
+          <Overlay
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-      <button onClick={nextPlease}>next</button>
-      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 }
